736fd76ef22520a9f0f629045eecdd6bf03b66d1
LLL <18370791453@163.com>
2019-01-21 19:03:46 +0800

    456

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 // 引入Details
 import Details from './pages/Details.vue'
 
-//引入Vant 为了不让他报错我改下下面的写法
+//引入Vant 为了不让他报错我改下下面的写法1564523
 // import Vant from 'vant';
 import 'vant'
 import 'vant/lib/index.css';
