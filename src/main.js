70c744e3d5de9ab86a178233026a856f048f749f
ll <304566206@qq.com>
2019-01-21 18:51:50 +0800

    添加了路由

--- src/main.js
+++ src/main.js
@@ -1,14 +1,34 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Vant from 'vant';
+// 引入Details
+import Details from './pages/Details.vue'
+
+//引入Vant 为了不让他报错我改下下面的写法
+// import Vant from 'vant';
+import 'vant'
 import 'vant/lib/index.css';
 import { Swipe, SwipeItem } from 'vant';
 import './css/index.css'
 
+//这个是引入需求的vant组件
 Vue.use(Swipe).use(SwipeItem);
 
-Vue.config.productionTip = false
+//引入路由
+import VueRouter from 'vue-router'
+Vue.use(VueRouter);
+//注册路由
+const routes = [
+    {path : '/',component:App},
+    {path : '/details',component:Details}
+];
+//创建一个VueRouter的实例
+let router = new VueRouter({
+    routes
+});
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h('router-view'),
+    router
+}).$mount('#app');
