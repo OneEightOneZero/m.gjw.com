391b4d70af6cf73c2647bde7acd9f7e72e9ffa1e
用户名 <76575@qq.com>
2019-01-22 21:42:08 +0800

    列表页

--- src/main.js
+++ src/main.js
@@ -3,6 +3,22 @@ import App from './App.vue'
 // 引入Details
 import Details from './pages/Details.vue'
 
+
+//列表页
+import List from './pages/List.vue'
+//引进axios
+import axios from "axios"
+Vue.prototype.$axios=axios;
+//引进jq
+import $ from "jquery"
+Vue.prototype.$=$;
+//下拉刷新
+import { PullRefresh } from 'vant';
+
+Vue.use(PullRefresh)
+
+
+
 //引入Vant 为了不让他报错我改下下面的写法1564523
 // import Vant from 'vant';
 import 'vant'
@@ -19,7 +35,8 @@ Vue.use(VueRouter);
 //注册路由
 const routes = [
     {path : '/',component:App},
-    {path : '/details',component:Details}
+    {path : '/details',component:Details},
+	{path : '/list',component:List}
 ];
 //创建一个VueRouter的实例
 let router = new VueRouter({
