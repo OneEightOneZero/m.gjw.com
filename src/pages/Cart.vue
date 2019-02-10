c37c7ce38e57f4d061c9113160c93d38894d79b7
LLL <18370791453@163.com>
2019-02-10 19:27:52 +0800

    修复购物车bug

--- src/pages/Cart.vue
+++ src/pages/Cart.vue
@@ -6,7 +6,7 @@
 </template>
 <script>
 import LcartHearder from "../components/LcartHaeder.vue";
-// import LcartBody from "../components/LcartBody.vue";
+// import LcartBody from "../components/LcartBody.vue1";
 import LcartBody from "../components/Lcart.vue";
 
 
